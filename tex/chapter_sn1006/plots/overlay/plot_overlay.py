e1dc132419ce70394c3e41f2c5258d2b2aec9b5d
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-07-20 21:56:40 +1000

    half through dalek chapter. still a lot to do, but the rest is outlined.

---
+++ tex/chapter_sn1006/plots/overlay/plot_overlay.py
@@ -0,0 +1,28 @@
+import aplpy,ephem
+data=recfromtxt('SN1006DB.dat',converters={1:ephem.hours,2:ephem.degrees},names=True)
+
+ra=data['ra']/ephem.degree
+dec=data['dec']/ephem.degree
+clf()
+dss=aplpy.FITSFigure('dss.fits',figure=figure(1))
+dss.show_grayscale(invert='True')
+dss.show_contour('vla.fits',colors='black',linewidths=3,linestyles='-')
+dss.show_markers(ra,dec,s=50,zorder=20,linewidths=3)
+
+sm_star_coord = (ephem.hours('15:02:53.15')/ephem.degree, ephem.degrees('-41:59:16.7')/ephem.degree)
+
+xlim(418.25256783519171, 976.38449164673648)
+ylim(1.3028136019328969, 565.40172799399215)
+dss.show_markers(ra,dec,s=20,zorder=20,linewidths=3)
+dss.show_markers([sm_star_coord[0]], [sm_star_coord[1]], color='blue')
+title('SN1006 DSS Image with VLA Overlay (Overlay is cutoff)',fontsize=20)
+ax=gca()
+ax.set_xlabel("RA (J2000)",fontsize=20)
+ax.set_ylabel("Dec (J2000)",fontsize=20)
+for item in ax.get_xticklabels():
+	item.set_fontsize(15)
+for item in ax.get_yticklabels():
+	item.set_fontsize(15)
+savefig('sn1006_overlay.pdf')
+savefig('sn1006_overlay.png')
+
---
+++ tex/chapter_sn1006/plots/plot_grid_extent.py
@@ -0,0 +1,2 @@
+import sqlite3
+conn = sqlite3.connect('/Users/wkerzend/tmp/SN1006/grid/vald_marcs_molec7/index.db3')
